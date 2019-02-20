4343266ac0c1b8b2c1920994e5ffe3fab4de67bb
manishkumr <rishimanish123@gmail.com>
2019-02-20 12:44:58 +1100

    react state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import createReactClass from 'create-react-class'
+import PropTypes from 'prop-types'
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+/**
+var component = createReactClass({
+  render: function() {
+    return <span>My name is  {this.props.name} </span>;
+  }
+});
+var ComponentFactory = React.createFactory(component)
+ReactDOM.render(
+     React.createElement(component,{name: "Manish"}),
+    document.getElementById("app")
+);
+**/
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var TextAreaCounter = createReactClass({
+    propTypes: {
+        text : PropTypes.string,
+    },
+    getDefaultProps: function () {
+      return {
+          text: '',
+      };
+    },
+    getInitialState: function () {
+        return {
+            text: this.props.text
+        };
+    },
+    _textChange: function(ev) {
+      this.setState({
+          text: ev.target.value,
+          }
+
+      )
+    },
+    render: function () {
+        return <div>
+            <textarea onChange={this._textChange} value={this.state.text}> </textarea>
+            <h3>{this.state.text.length}</h3>
+        </div>
+
+    }
+});
+
+var myComp = ReactDOM.render(
+     React.createElement(TextAreaCounter,{
+        text: "Manish",
+     }),
+    document.getElementById("app")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
